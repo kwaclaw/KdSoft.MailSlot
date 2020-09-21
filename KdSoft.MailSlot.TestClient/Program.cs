c9d241fdefe90753b9cf59057c5e14d0d98594ba
Karl Waclawek <karl@waclawek.net>
2020-09-21 12:03:04 -0400

    Addressed some potential issues with FlieStream.FlushAsync().

--- KdSoft.MailSlot.TestClient/Program.cs
+++ KdSoft.MailSlot.TestClient/Program.cs
@@ -12,11 +12,16 @@ namespace KdSoft.MailSlot.TestClient {
             try {
                 using (var client = MailSlot.CreateClient("test1")) {
                     for (int indx = 0; indx < 50; indx++) {
-                        var count = Encoding.UTF8.GetBytes($"Writing line #{indx}.", buffer);
+                        var msg = $"Writing line #{indx}.";
+                        // var count = Encoding.UTF8.GetBytes(msg, 0, msg.Length, buffer, 0);
+                        var count = Encoding.UTF8.GetBytes(msg, buffer);
                         buffer[count++] = MessageSeparator;
                         await Task.Delay(100);  // small delay between messages
                         await client.WriteAsync(buffer, 0, count);
-                        await client.FlushAsync();
+
+                        // This leads to "Incorrect Function" error on full framework:
+                        //     client.Flush(true) or await client.FlushAsync();
+                        client.Flush(false);
                     }
                 }
             }
